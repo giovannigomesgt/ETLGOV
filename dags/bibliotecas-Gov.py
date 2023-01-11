61cad508b1c918ac8bf6145514c4dfed32ea48cf
Giovanni Leocádio <giovannigomes.snt@gmail.com>
2023-01-11 18:21:31 -0300

    #8

--- dags/bibliotecas-Gov.py
+++ dags/bibliotecas-Gov.py
@@ -1,20 +1,30 @@
 from airflow.operators.bash_operator import BashOperator
-from airflow.decorators import dag, task
+from airflow.operators.dummy import DummyOperator
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import BranchPythonOperator
 
 
-@dag(
-    schedule_interval=None,
-    catchup=False,
-    tags=['BIBLIOTECAS'])
+
 
 def bibliotecas():
-    pass
-install_pandas = BashOperator(
-    task_id='install_tqdm',
-    bash_command='pip install tqdm',   
-)
+    return 'install_tqdm'
 
 
+with DAG('bibliotecas', start_date=datetime(2022,12,16),
+    schedule_interval=None, catchup= False, tags=['Bibliotecas']) as dag:
 
 
-install_pandas
+    taskbibioteca = BranchPythonOperator(
+        task_id = 'func_biblioteca',
+        python_callable = bibliotecas
+    )
+
+    install_pandas = BashOperator(
+        task_id='install_tqdm',
+        bash_command='pip install tqdm',   
+    )
+
+    fim =  DummyOperator(task_id = "fim")
+
+taskbibioteca >> fim
