11e361316dfae48d15797dedc3727fb93341d0b3
Giovanni Leocádio <giovannigomes.snt@gmail.com>
2023-01-23 15:34:52 -0300

    #20

--- dags/de_dependencias.py
+++ dags/de_dependencias.py
@@ -1,12 +1,15 @@
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
+from datetime import datetime
 
 def install_dependencies():
     import subprocess
     subprocess.call(["pip", "install", "tqdm"])
 
-dag = DAG(dag_id="install_dependencies", schedule_interval=None)
+dag = DAG(dag_id="install_dependencies",
+    start_date=datetime.now(),
+    schedule_interval=None)
 
 inicio =  DummyOperator(task_id = "inicio")
 fim =  DummyOperator(task_id = "fim")
